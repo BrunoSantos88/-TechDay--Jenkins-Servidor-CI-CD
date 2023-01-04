8e362641fb249f205f9ddb19f296b31d36d455a1
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-04 19:27:43 -0300

    jenkinsfile

--- Jenkinsfile.sh
+++ Jenkinsfile.sh
@@ -1,13 +1,29 @@
 pipeline {
-    agent any
+  agent any
     environment {
         AWS_ACCOUNT_ID="555527584255"
         AWS_DEFAULT_REGION="us-east-1"
         IMAGE_REPO_NAME="asg"
         IMAGE_TAG="v1"
         REPOSITORY_URI = "555527584255.dkr.ecr.us-east-1.amazonaws.com/asg"
+  tools { 
+        maven 'Maven 3.5.2'  
     }
-   
+   stages{
+    stage('SonarCloud-GateCode-Quality') {
+            steps {	
+		sh 'mvn clean verify sonar:sonar -Dsonar.projectKey=-TechDay--Jenkins-Servidor-CI-CD -Dsonar.organization=brunosantos88-1 -Dsonar.host.url=https://sonarcloud.io -Dsonar.login=700dcb9a79ba7aa525cdf858e19ccf6ad1e59b98'
+			}
+        } 
+
+stage('Synk-GateSonar-Security') {
+            steps {		
+				withCredentials([string(credentialsId: 'SNYK_TOKEN', variable: 'SNYK_TOKEN')]) {
+					sh 'mvn snyk:test -fn'
+				}
+			}
+  }
+
     stages {
         
          stage('Logging into AWS ECR') {
@@ -19,11 +35,6 @@ pipeline {
             }
         }
         
-        stage('Cloning Git') {
-            steps {
-                checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '', url: 'https://github.com/sd031/aws_codebuild_codedeploy_nodeJs_demo.git']]])     
-            }
-        }
   
     // Building Docker images
     stage('Building image') {
@@ -43,5 +54,7 @@ pipeline {
          }
         }
       }
+    }
+}
     }
 }
