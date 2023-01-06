e3e623f387e2175ae99dacfbbe066bd3af774809
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-06 16:48:15 -0300

    dockerfile

--- Scripts/jenkis-amz.sh
+++ Scripts/jenkis-amz.sh
@@ -19,6 +19,7 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 sudo yum install unzip
 sudo unzip awscliv2.zip  
 sudo ./aws/install
+
 #ZAP is isntalled at /home/ec2-user/ZAP_2.11.1/zap.sh
 sudo wget https://github.com/zaproxy/zaproxy/releases/download/v2.11.1/ZAP_2_11_1_unix.sh
 sudo chmod +x ZAP_2_11_1_unix.sh 
@@ -28,8 +29,10 @@ curl -o kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.23.7/2022-06-29/
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
 sudo cp kubectl /usr/local/bin/
+
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
+
 sudo yum install docker -y
 sudo usermod -aG docker $USER
 sudo newgrp docker
