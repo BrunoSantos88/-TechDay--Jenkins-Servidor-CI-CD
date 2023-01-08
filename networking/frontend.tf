60085fc9d7209d30de93c0d7fc230b74765effb8
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-08 17:31:46 -0300

    repos

--- networking/frontend.tf
+++ networking/frontend.tf
@@ -11,7 +11,7 @@ resource "aws_vpc" "vpc" {
   enable_dns_support   = true
 
   tags = {
-    Name = "Developer-${random_integer.random.id}"
+    Name = "Frontend-${random_integer.random.id}"
   }
 }
 
@@ -23,20 +23,8 @@ resource "aws_subnet" "public_subnet" {
   availability_zone       = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"][count.index]
 
   tags = {
-    Name = "frontend_${count.index + 1}"
+    Name = "frontend_${count.index + 2}"
   }
-
-  resource "aws_subnet" "public_subnet" {
-  count                   = length(var.public_cidrs)
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public_cidrs[count.index]
-  map_public_ip_on_launch = true
-  availability_zone       = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d", "us-east-1e", "us-east-1f"][count.index]
-
-  tags = {
-    Name = "backend_${count.index + 1}"
-  }
-}
 }
 
 resource "aws_route_table_association" "public_assoc" {
