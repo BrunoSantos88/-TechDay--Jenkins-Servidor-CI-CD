34da2de83d2db12e139d672420f1d36fe9b5e116
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-08 16:50:28 -0300

    repos

--- main.tf
+++ main.tf
@@ -3,9 +3,3 @@ module "networking" {
   vpc_cidr     = "10.0.0.0/16"
   public_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
 }
-
-module "compute" {
-  source        = "./compute"
-  web_sg        = module.networking.web_sg
-  public_subnet = module.networking.public_subnet
-}
