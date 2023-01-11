c4eef8bf81ab2a8187b772be40db009a7897390d
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-11 10:45:34 -0300

    terraforfile

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 module "networking" {
-  source       = "./networking"
+  source       = "./-Terraform/networking"
   vpc_cidr     = "10.0.0.0/16"
   public_cidrs = ["10.0.1.0/24", "10.0.2.0/24","10.0.3.0/24"]
+}
+
+module "networking" {
+  source       = "./-Terraform/compute"
+  
 }
