49179339db925d1eb884f17cfdc83b40345c3369
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-13 10:54:47 -0300

    terraformfile

--- ProjetoJskinsSonar/outputs.tf
+++ ProjetoJskinsSonar/outputs.tf
@@ -5,3 +5,20 @@ output "vpc_id" {
 output "jks-sg" {
   value = aws_security_group.jks-sg.id
 }
+
+output "jks-sg" {
+  value = aws_db_subnet_group.networkingdb.id
+}
+
+
+output "instance-jenkis" {
+  value = aws_instance.jks.id
+}
+
+output "instance-sonar" {
+  value = aws_instance.SonarQube.subnet_id
+}
+output "sec-group" {
+  value = aws_security_group.jks-sg.id
+}
+
