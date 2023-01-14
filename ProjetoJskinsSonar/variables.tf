870f461625d49f93dd4e5959825cea3a3c6343e7
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-14 12:13:15 -0300

    Jenkinsfile

--- ProjetoJskinsSonar/variables.tf
+++ ProjetoJskinsSonar/variables.tf
@@ -1,9 +1,4 @@
 # --- comput/variables.tf ---
-variable "Sonar_instance_type" {
-  type    = string
-  default = "t2.large"
-}
-
 variable "Jenkis_instance_type" {
   type    = string
   default = "t2.large"
