197bdbaa78812a94274ce7bc34819181917b1c92
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-14 03:06:47 -0300

    Jenkinsfile

--- ProjetoJskinsSonar/outputs.tf
+++ ProjetoJskinsSonar/outputs.tf
@@ -6,17 +6,4 @@ output "security-group" {
   value = aws_security_group.jks-sg.id
 }
 
-output "subnet_db" {
-  value = aws_db_subnet_group.net-dados.id
-}
-
-
-output "instance-jenkis" {
-  value = aws_instance.jks.id
-}
-
-output "instance-sonar" {
-  value = aws_instance.SonarQube.subnet_id
-}
-
 
