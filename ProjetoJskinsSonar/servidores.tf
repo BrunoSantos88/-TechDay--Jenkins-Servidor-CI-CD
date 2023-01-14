92fa9745ae3e6b99c66bbc81a67218b7db7a570f
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-14 02:35:17 -0300

    Jenkinsfile

--- ProjetoJskinsSonar/servidores.tf
+++ ProjetoJskinsSonar/servidores.tf
@@ -31,38 +31,3 @@ resource "aws_instance" "jks" {
 
  user_data = filebase64("Scripts/jenkis-amz.sh")  ##SHELLSCRIPT
 }
-
-
-resource "aws_instance" "SonarQube" {
-  ami                         = "ami-0b5eea76982371e91"  #mazonlinuz2
-  instance_type               = var.Sonar_instance_type  #T2 LARGE
-  availability_zone           = "us-east-1b"
-  subnet_id                   = aws_subnet.jks_subnet_public_1b.id
-  associate_public_ip_address = true
-  key_name                    = "minhaaws"  #CHAVE SSH
-
-
-  vpc_security_group_ids = [    ## portas 80, 8081, 22
-   aws_security_group.jks-sg.id
-  ]
-
-  root_block_device {
-    delete_on_termination = true
-    volume_size           = 50  #SSD
-    volume_type           = "gp2"
-
-
-    tags = {
-      Name = "SonarQube_Sever"
-    }
-  }
-
-  tags = {
-    Name        = "Sonar*Server"
-    Environment = "Treinamento"
-    OS          = "AMAZONLINUX2"
-    Managed     = "IAC"
-  }
-
- user_data = filebase64("Scripts/sonaqub.sh")  ##SHELLSCRIPT
-}
