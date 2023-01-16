4edaef6d2eb1cc51279f0ed88d65f4d839dbd82c
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-16 11:28:29 -0300

    filesupload

--- ecr/iam.ecr.tf
+++ ecr/iam.ecr.tf
@@ -23,29 +23,3 @@ resource "aws_ecr_repository_policy" "frontend" {
   }
   EOF
 }
-
-resource "aws_ecr_repository_policy" "backend-repo-policy" {
-  repository = aws_ecr_repository.ecr_back.name
-  policy     = <<EOF
-  {
-    "Version": "2008-10-17",
-    "Statement": [
-      {
-        "Sid": "adds full ecr access to the demo repository",
-        "Effect": "Allow",
-        "Principal": "*",
-        "Action": [
-          "ecr:BatchCheckLayerAvailability",
-          "ecr:BatchGetImage",
-          "ecr:CompleteLayerUpload",
-          "ecr:GetDownloadUrlForLayer",
-          "ecr:GetLifecyclePolicy",
-          "ecr:InitiateLayerUpload",
-          "ecr:PutImage",
-          "ecr:UploadLayerPart"
-        ]
-      }
-    ]
-  }
-  EOF
-}
