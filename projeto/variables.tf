e01b9e07f4904901ba8c6340a58762cfbe956034
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-16 14:20:35 -0300

    filesupload

--- projeto/variables.tf
+++ projeto/variables.tf
@@ -3,18 +3,3 @@ variable "monitoring_instance_type" {
   type    = string
   default = "t2.large"
 }
-
-variable "ecr-front" {
- description = "frontend"
- type = string
-}
-
-variable "ecr-back" {
- description = "backend"
- type = string
-}
-
-variable "region" {
- description = "us-east-1"
- type = string
-}
