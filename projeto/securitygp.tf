a53434d9c5b8a8bca844a2d218618c6e498239ba
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-17 12:27:01 -0300

    fileupload

--- projeto/securitygp.tf
+++ projeto/securitygp.tf
@@ -3,8 +3,8 @@ resource "aws_security_group" "sg-networking" {
   vpc_id = aws_vpc.networking.id
   
   ingress {
-    from_port   = "8080"
-    to_port     = "8080"
+    from_port   = "8081"
+    to_port     = "8081"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
