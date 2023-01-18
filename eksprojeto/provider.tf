0830948881c5328bde1b9030ded835dd6f150f5d
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-18 13:23:49 -0300

    helmprojeto

---
+++ eksprojeto/provider.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+    required_version = "1.3.7"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
