7f72f50917b09a6fb5bacb79dd471ffec99586a5
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-17 22:57:36 -0300

    uploadfile

--- projeto/eks.tf
+++ projeto/eks.tf
@@ -7,6 +7,7 @@ resource "aws_eks_cluster" "aws-eks" {
     Name = "aws-eks"
   }
 }
+
 resource "aws_eks_node_group" "node1" {
   cluster_name    = aws_eks_cluster.aws-eks.name
   node_group_name = "eks-cluster"
@@ -14,7 +15,7 @@ resource "aws_eks_node_group" "node1" {
   subnet_ids      = [aws_subnet.subnet_public_1a.id]
   remote_access {
   ec2_ssh_key	  = "minhaaws"
-source_security_group_ids = [aws_security_group.sg-networking.id]
+  source_security_group_ids = [aws_security_group.sg-networking.id]
   }
   disk_size		  =50
   scaling_config {
@@ -22,9 +23,6 @@ source_security_group_ids = [aws_security_group.sg-networking.id]
     max_size     = 1
     min_size     = 1
   }
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  
 }
 resource "aws_eks_node_group" "node2" {
   cluster_name    = aws_eks_cluster.aws-eks.name
