23510bfb73ccf88b23c76184edf3616ab45e96ee
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-18 10:48:03 -0300

    projetoeks

--- eksprojeto/node.tf
+++ eksprojeto/node.tf
@@ -9,11 +9,10 @@ resource "aws_eks_node_group" "example" {
     max_size     = 1
     min_size     = 1
   }
-
-    eks_managed_node_group_defaults = {
+  
     ami_type       = "AL2_x86_64"
     instance_types = ["t3.medium"]
-  }
+  
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
