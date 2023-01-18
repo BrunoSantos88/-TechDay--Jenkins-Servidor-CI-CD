ac87d88000e3c41297f3462534afdf3a84f6af95
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-18 12:14:51 -0300

    projetoeks

--- main.tf
+++ main.tf
@@ -11,13 +11,3 @@ module "eksbasic" {
 source    = "./eksprojeto" 
 
 }
-
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
