a69b77c75ade6a1b5a15f748481d5f573f01f10f
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-18 11:48:22 -0300

    projetoeks

--- eksprojeto/providers.tf
+++ eksprojeto/providers.tf
@@ -5,8 +5,3 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
-
-provider "kubernetes" {
-  host                   = module.eks.cluster_endpoint
-  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-}
