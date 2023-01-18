7096932d0869b781b31cc6d9a97910c22e5b9f5c
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-17 22:54:32 -0300

    uploadfile

--- projeto/eks.tf
+++ projeto/eks.tf
@@ -1,6 +1,5 @@
 resource "aws_eks_cluster" "aws-eks" {
   name     = "aws-eks"
-  role_arn = aws_iam_role.eks_cluster.arn
   vpc_config {
     subnet_ids = [aws_subnet.subnet_public_1c.id,aws_subnet.subnet_public_1a.id,aws_subnet.subnet_public_1b.id]
   }
@@ -12,7 +11,6 @@ resource "aws_eks_node_group" "node1" {
   cluster_name    = aws_eks_cluster.aws-eks.name
   node_group_name = "eks-cluster"
   instance_types	  =["t2.medium"]
-    node_role_arn   = aws_iam_role.eks_nodes.arn
   subnet_ids      = [aws_subnet.subnet_public_1a.id]
   remote_access {
   ec2_ssh_key	  = "minhaaws"
@@ -26,17 +24,12 @@ source_security_group_ids = [aws_security_group.sg-networking.id]
   }
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-  ]
+  
 }
 resource "aws_eks_node_group" "node2" {
   cluster_name    = aws_eks_cluster.aws-eks.name
   node_group_name = "ng-2"
   instance_types	  =["t2.medium"]
-    node_role_arn   = aws_iam_role.eks_nodes.arn
   subnet_ids      = [aws_subnet.subnet_public_1b.id]
   remote_access {
   ec2_ssh_key	  = "minhaaws"
@@ -48,9 +41,4 @@ source_security_group_ids = [aws_security_group.sg-networking.id]
     max_size     = 1
     min_size     = 1
   }
-  depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-  ]
 }
