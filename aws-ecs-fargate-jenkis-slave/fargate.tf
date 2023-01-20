10df89464966d213ec521336111bb4ec4a68b878
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-20 19:04:44 -0300

    jenkins-slave

--- aws-ecs-fargate-jenkis-slave/fargate.tf
+++ aws-ecs-fargate-jenkis-slave/fargate.tf
@@ -1,9 +1,11 @@
 resource "aws_ecs_task_definition" "definition" {
   family                   = "task_definition_name"
-  task_role_arn            = "${var.ecs_task_role}"
-  execution_role_arn       = "${var.ecs_task_execution_role}"
+  task_role_arn            = aws_iam_role.ecs_task_role.id
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.id
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "1024"
   requires_compatibilities = ["FARGATE"]
   }
+
+  
