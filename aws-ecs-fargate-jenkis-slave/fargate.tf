c32f9d9275c8b763f7718d3165a5615950dadd0d
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-20 19:37:41 -0300

    jenkins-slave

--- aws-ecs-fargate-jenkis-slave/fargate.tf
+++ aws-ecs-fargate-jenkis-slave/fargate.tf
@@ -2,14 +2,14 @@ resource "aws_ecs_task_definition" "jkstask" {
   family                        = "jsslave"
   network_mode                  = "awsvpc"
   requires_compatibilities      = ["FARGATE", "EC2"]
-  cpu                           = 512
-  memory                        = 2048
+  cpu                           = 1024
+  memory                        = 3072
   container_definitions         = jsonencode([
     {
       name      = "jenkins-slave"
       image     = "brunosantos88/jenkins-slave:latest"
-      cpu       = 512
-      memory    = 2048
+      cpu       = 1024
+      memory    = 3072
       essential = true  # if true and if fails, all other containers fail. Must have at least one essential
       portMappings = [
         {
