6e35a6e0e323a741fc3c2964d098639ec0399f40
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-25 17:25:54 -0300

    tffile

--- aws-projetofinal/sonarfargate.tf
+++ aws-projetofinal/sonarfargate.tf
@@ -3,13 +3,13 @@ resource "aws_ecs_task_definition" "sonar" {
   network_mode                  = "awsvpc"
   requires_compatibilities      = ["FARGATE", "EC2"]
   cpu                           = 2048
-  memory                        = 4000
+  memory                        = 3072
   container_definitions         = jsonencode([
     {
       name      = "sonarquebe"
       image     = "brunosantos88/awssonarqube:latest"
       cpu       = 2048
-      memory    = 4000
+      memory    = 3072
       essential = true  # if true and if fails, all other containers fail. Must have at least one essential
       portMappings = [
         {
