10fdf92aa745cc6ecc02f58e63ef4a792f2daa6b
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-25 17:08:13 -0300

    tffile

---
+++ aws-projetofinal/service.tf
@@ -0,0 +1,15 @@
+resource "aws_ecs_service" "sonarquebe" {
+  name              = "sonar"
+  cluster           = aws_ecs_cluster.jenkslvn.id
+  task_definition   = aws_ecs_task_definition.jkstask.id
+  desired_count     = 1
+  launch_type       = "FARGATE"
+  platform_version  = "LATEST"
+
+  network_configuration {
+    assign_public_ip  = true
+    security_groups   = [aws_security_group.sg-networking.id]
+    subnets           = [aws_subnet.subnet_public_1a.id,aws_subnet.subnet_public_1b.id,aws_subnet.subnet_public_1c.id]
+  }
+  
+  }
