3533455d70df18397ad20df20a50aafd8f9786bc
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-25 16:17:32 -0300

    jffile

--- aws-projetofinal/node.tf
+++ aws-projetofinal/node.tf
@@ -72,18 +72,3 @@ resource "aws_eks_node_group" "public-nodes" {
     aws_iam_role_policy_attachment.nodes-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
-
- resource "aws_launch_template" "eks-with-disks" {
-   name = "eks-with-disks"
-
-   key_name = "minhaaws"
-
-   block_device_mappings {
-    device_name = "/dev/xvdb"
-
-    ebs {
-     volume_size = 50
-    volume_type = "gp2"
-    }
-  }
- }
