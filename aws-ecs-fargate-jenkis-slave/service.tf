ab2bb7a9a9c66007c924aa4eebf87da52e0b6e15
BrunoSantos <91704169+BrunoSantos88@users.noreply.github.com>
2023-01-25 23:55:13 -0300

    jffile

--- aws-ecs-fargate-jenkis-slave/service.tf
+++ aws-ecs-fargate-jenkis-slave/service.tf
@@ -1,5 +1,5 @@
-resource "aws_ecs_service" "jkslve" {
-  name              = "jsnkislv"
+resource "aws_ecs_service" "sonarquebe" {
+  name              = "sonar"
   cluster           = aws_ecs_cluster.jenkslvn.id
   task_definition   = aws_ecs_task_definition.jkstask.id
   desired_count     = 1
